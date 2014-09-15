a5e08d461cfb430b7d48c7cc2386fa80e53ac0d5
Luka111 <lukapetrovicsi@gmail.com>
2014-09-15 21:54:43 +0200

    And the custom openlayers

---
+++ bower_components/openlayers/src/ol/source/wmssource.js
@@ -0,0 +1,13 @@
+goog.provide('ol.source.wms');
+goog.provide('ol.source.wms.ServerType');
+
+
+/**
+ * @enum {string}
+ */
+ol.source.wms.ServerType = {
+  CARMENTA_SERVER: 'carmentaserver',
+  GEOSERVER: 'geoserver',
+  MAPSERVER: 'mapserver',
+  QGIS: 'qgis'
+};
